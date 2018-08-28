f616a1dad2a90733f57318e31aaf2fa287ad785f
hawkit <terryc007@gmail.com>
2018-08-28 17:32:35 +0800

    add format, string function for AmountUnit, Amount

--- amount.go
+++ amount.go
@@ -1,6 +1,9 @@
 package btcutil
 
-import "math"
+import (
+	"math"
+	"strconv"
+)
 
 // AmountUnit describes a method of covertin an Amount to something
 // other than the base unit of a bition. The value of the AmountUnit
@@ -24,6 +27,25 @@ const (
 	AmountSatoshi AmountUnit = -8
 )
 
+func (u AmountUnit) String() string {
+	switch u {
+	case AmountMegaBTC:
+		return "MBTC"
+	case AmountKiloBTC:
+		return "KBTC"
+	case AmountBTC:
+		return "BTC"
+	case AmountMilliBTC:
+		return "mBTC"
+	case AmountMicroBTC:
+		return "μBTC"
+	case AmountSatoshi:
+		return "Satoshi"
+	default:
+		return "1e" + strconv.FormatInt(int64(u), 10) + "BTC"
+	}
+
+}
 // ToUnit converts a monetary amount counted in bitcoin base units to
 // a floating point value representing an amount of bitcoin
 func (a Amount) ToUnit(u AmountUnit) float64  {
@@ -33,4 +55,18 @@ func (a Amount) ToUnit(u AmountUnit) float64  {
 // ToBTC is the equivalent of calling ToUnit with AmountBTC.
 func (a Amount) ToBTC() float64 {
 	return a.ToUnit(AmountBTC)
+}
+
+// Format formats a moneytary amount counted in bitcoin base units as
+// a string for a given unit. The conversion will succeed for any unit,
+// however, known units will be formated with an appended label describing
+// the units with SI notation, or "Sataoshi" for the base unit.
+func (a Amount) Format(u AmountUnit) string {
+	units := " " + u.String()
+	return  strconv.FormatFloat(a.ToUnit(u), 'f', -int(u+8), 64) + units
+}
+
+// String is the equivalent of calling Format with AmountBTC
+func (a Amount) String() string  {
+	return a.Format(AmountBTC)
 }
