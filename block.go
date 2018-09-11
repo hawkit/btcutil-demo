4f5e9b5b44d3c9fcf5c6b66fcd0ffe4d5efcfd2e
hawkit <terryc007@gmail.com>
2018-09-11 17:32:27 +0800

    add Bytes() function for Block

--- block.go
+++ block.go
@@ -4,6 +4,7 @@ import (
 
 	"github.com/hawkit/btcd-demo/wire"
 	"github.com/hawkit/btcd-demo/chaincfg/chainhash"
+	"bytes"
 )
 
 // Block defines a bitcoin block that provides easier and more efficent
@@ -18,4 +19,28 @@ type Block struct {
 	blockHeight int32 // Height in the main block chain
 	transactions []*Tx // Transactions
 	txnsGenerated bool // All wrapped transactions generated
+}
+
+// Bytes returns the serialized bytes for the Block. This is equivalent to
+// calling Serialize on the underlying wire.MsgBlock, however it caches the
+// result so subsequent calls are more efficient.
+func (b *Block) Bytes() ([]byte, error)  {
+
+	// Return the cached serialized bytes if it has already been generated.
+	if len(b.serializedBlock) != 0 {
+		return b.serializedBlock, nil
+	}
+
+	// Serialize the MsgBlock.
+	w := bytes.NewBuffer(make([]byte, 0, b.MsgBlock.SerializeSize()))
+
+	err := b.MsgBlock.Serialize(w)
+	if err != nil {
+		return nil, err
+	}
+
+	serailzedBlock := w.Bytes()
+	b.serializedBlock = serailzedBlock
+	return serailzedBlock, nil
+
 }
